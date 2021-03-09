29cdfecac2c3e482b8860380acc2495bbafff1ae
runacode <59107019+runacode@users.noreply.github.com>
2021-03-09 14:43:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php $company = "DAHL S AUTO WORKS INC";
-$phone = "(507) 864-7711";
-	$address1 = "207 N ELM ST";
-	$address2 = "RUSHFORD, MN 55971";
+<?php $company = "SLD AUTO SALES, L.L.C.";
+$phone = "(410) 238-7500";
+	$address1 = "527 EASTERN BLVD";
+	$address2 = "ESSEX, MD 21221";
 ?>
 <!DOCTYPE html>
 <html style="font-size: 16px;">
@@ -73,8 +73,8 @@ $phone = "(507) 864-7711";
                     
                   </div>
                 </div>
-                <h3 class="u-text u-text-7">Contact us</h3>
-                <h6 class="u-text u-text-8">Tell us about your project!</h6>
+                <h3 class="u-text u-text-7">Hi</h3>
+                <h6 class="u-text u-text-8">Let's work together!</h6>
               </div>
             </div>
           </div>
